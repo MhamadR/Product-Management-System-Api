270656fd1e7fcb9ec1e5d632b3cd480e63e7faae
Mhamad Raslan <mhamadraslan@gmail.com>
2023-08-06 20:19:29 +0300

    Add database connection with PDO

---
+++ src/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+class database
+{
+    public function __construct(
+        private $host,
+        private $name,
+        private $user,
+        private $password
+    ) {
+    }
+
+    public function getConnection(): PDO
+    {
+        $dsn = "mysql:host={$this->host}; dbname={$this->name}; charset=utf8";
+
+        return new PDO($dsn, $this->user, $this->password, [
+            // Set the following PDO attributes
+            // To not let PDO convert all values to string
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_STRINGIFY_FETCHES => false,
+        ]);
+    }
+}
