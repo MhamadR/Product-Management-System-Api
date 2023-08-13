15c329e9f05e47b11c21cd5922ab4a92eb7edb02
Mhamad Raslan <mhamadraslan@gmail.com>
2023-08-13 21:50:45 +0300

    Implement HTTP message interfaces and validation

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 declare(strict_types=1);
 
 use Dotenv\Dotenv;
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\ServerRequestFactory;
 use TestAssignment\src\ErrorHandler;
 use TestAssignment\src\database;
 use TestAssignment\src\ProductGateway;
 use TestAssignment\src\ProductController;
 use TestAssignment\src\ProductRepository;
+use TestAssignment\src\ProductValidator;
 
 require_once('vendor/autoload.php');
 
@@ -17,21 +20,23 @@ $dotenv->load();
 set_error_handler([ErrorHandler::class, 'handleError']);
 set_exception_handler([ErrorHandler::class, 'handleException']);
 
-
 header("Content-type: application/json; charset=UTF-8");
 // Enable CORS headers
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+$request = ServerRequestFactory::fromGlobals();
+$response = new Response();
+
 // Check if it's a preflight request
-if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+if ($request->getMethod() === "OPTIONS") {
     header("HTTP/1.1 204 No Content");
     exit();
 }
 
 // Divide the Request URI string into an array of substrings based on the separator "/"
-$parts = explode("/", $_SERVER["REQUEST_URI"]);
+$parts = explode("/", $request->getUri()->getPath());
 
 // Throw 404 code when the URI is different than "/products or "/products/"
 if ($parts[2] !== "products" || (isset($parts[3]) && $parts[3] !== "")) {
@@ -45,6 +50,17 @@ $gateway = new ProductGateway($database);
 
 $repository = new ProductRepository($gateway);
 
-$controller = new ProductController($repository);
+$validator = new ProductValidator($repository);
 
-$controller->processRequest($_SERVER["REQUEST_METHOD"]);
+$controller = new ProductController($repository, $validator);
+
+$response = $controller->processRequest($request, $response);
+http_response_code($response->getStatusCode());
+
+foreach ($response->getHeaders() as $name => $values) {
+    foreach ($values as $value) {
+        header(sprintf('%s: %s', $name, $value), false);
+    }
+}
+
+echo $response->getBody();
--- src/ProductController.php
+++ src/ProductController.php
@@ -2,186 +2,134 @@
 
 namespace TestAssignment\src;
 
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\ServerRequest;
+use Laminas\Diactoros\StreamFactory;
+use Psr\Http\Message\ResponseInterface;
+
 class ProductController
 {
-    public function __construct(private ProductGatewayInterface $gateway)
+    public function __construct(private ProductGatewayInterface $gateway, private ProductValidatorInterface $validator)
     {
     }
 
-    public function processRequest(string $method): void
+    public function processRequest(ServerRequest $request, Response $response): ResponseInterface
     {
+        $method = $request->getMethod();
         switch ($method) {
             case "GET":
-                echo json_encode($this->gateway->getAll());
+                return $this->handleGetRequest($response);
                 break;
             case "POST":
-                // cast json_decode to array will return empty array instead of null
-                //  when it is invalid or has no data
-                $data = (array) json_decode(file_get_contents("php://input"), true);
-
-                // Sanitize the input
-                $sanitizedData = $this->sanitizeInput($data);
-                if (!$sanitizedData) break;
-
+                $data = (array) json_decode(file_get_contents('php://input'));
                 // DELETE inside POST if "ids" property is set
                 if (array_key_exists("ids", $data)) {
-                    // Convert array of ids to a comma-separated string
-                    $idList = implode(",", $sanitizedData['ids']);
-                    // Delete products and associated records
-                    $result = $this->gateway->deleteProducts($idList);
-
-                    http_response_code(200);
-                    echo json_encode($result);
+                    return $this->handleDeleteRequest($data, $response);
                     break;
                 }
 
-                // Check if there are input errors
-                $errors = $this->getValidationErrors($sanitizedData);
-                if (!empty($errors)) {
-                    http_response_code(422);
-                    echo json_encode($errors);
-                    break;
-                }
-
-                $sku = $sanitizedData["sku"];
-                $name = $sanitizedData["name"];
-                $price = $sanitizedData["price"];
-                $type = $sanitizedData["type"];
-                $attributes = array_diff_key($sanitizedData, array_flip(["sku", "name", "price", "type"]));
-
-                // Check if the new product's sku already exists
-                if ($this->gateway->productExists($sku)) {
-                    http_response_code(409); // Conflict
-                    echo json_encode(["message" => "SKU already exists"]);
-                    break;
-                }
-
-                // Get product type id
-                $productTypeId = $this->gateway->lookupProductTypeId($type);
-
-
-                // Convert attributes to their relevant ids
-                $attributeIds = [];
-                foreach ($attributes as $attributeKey => $attributeValue) {
-                    $attributeId = $this->gateway->lookupAttributeId($attributeKey);
-                    $attributeIds[$attributeId] = $attributeValue;
-                }
-
-
-                $newData = [
-                    "sku" => $sku,
-                    "name" => $name,
-                    "price" => $price,
-                    "productTypeId" => $productTypeId,
-                    "attributeIds" => $attributeIds
-                ];
-
-                $id = $this->gateway->createProduct($newData);
-
-                http_response_code(201);
-                echo json_encode([
-                    "message" => "Product created",
-                    "id" => $id
-                ]);
+                return $this->handlePostRequest($data, $response);
                 break;
 
                 /*
                 case "DELETE":
-                    // Get data from the request body
-                    $data = (array) json_decode(file_get_contents("php://input"), true);
-                    $sanitizedData = $this->sanitizeInput($data);
-                    if (!$sanitizedData) break;
-
-                    // Convert array of ids to a comma-separated string
-                    $idList = implode(",", $sanitizedData['ids']);
-                    // Delete products and associated records
-                    $result = $this->gateway->deleteProducts($idList);
-
-                    http_response_code(200);
-                    echo json_encode($result);
+                    $data = (array) json_decode(file_get_contents('php://input'));
+                    $this->handleDeleteRequest($data, $response);
                     break;
                 */
-
-            default:
-                http_response_code(405);
-                header("Allow: GET, POST, DELETE");
         }
+        $responseBody = json_encode(['error' => 'Method Not Allowed']);
+        $stream = new StreamFactory();
+        $response = $response
+            ->withStatus(405) // Method Not Allowed
+            ->withHeader("Allow", "GET, POST, DELETE")
+            ->withBody($stream->createStream($responseBody));
+        return $response;
     }
 
-    private function sanitizeInput(array $data): array
+    private function handleGetRequest(Response $response): ResponseInterface
     {
-        $sanitizedData = [];
-        $validAttributes = $this->gateway->getAttributes();
-
-        if (array_key_exists("ids", $data)) {
-            // Check if the "ids" array is present and is an array
-            if (!isset($data["ids"]) || !is_array($data["ids"])) {
-                http_response_code(400); // Bad Request
-                echo json_encode(["message" => "Invalid request data. 'ids' must be an array of strings"]);
-                return [];
-            } else {
-                $sanitizedData["ids"] = array_map('intval', $data["ids"]);
-                return $sanitizedData;
-            }
-        }
-
-        foreach ($data as $key => $value) {
-            if (in_array($key, $validAttributes) || $key === "price") {
-                $sanitizedData[$key] = filter_var(trim($value), FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-            } else {
-                $sanitizedData[$key] = filter_var(trim($value), FILTER_SANITIZE_SPECIAL_CHARS);
-            }
-        }
-
-        return $sanitizedData;
+        $products = json_encode($this->gateway->getAll());
+        $stream = new StreamFactory();
+        $response = $response
+            ->withStatus(200)
+            ->withHeader('Content-Type', 'application/json')
+            ->withBody(($stream->createStream($products)));
+        return $response;
     }
 
-    private function getValidationErrors(array $data): array
+    private function handlePostRequest(array $data, Response $response): ResponseInterface
     {
-        $requiredKeys = ["sku", "name", "price", "type"];
-        $validTypes = $this->gateway->getTypes();
-        $validAttributes = $this->gateway->getAttributes();
-
-        $errors = [];
-
-        // Check if required keys exist or are empty
-        foreach ($requiredKeys as $key) {
-            if (!isset($data[$key]) || $data[$key] === "") {
-                $errors[$key] = "$key is required";
-            }
+        $sanitizedData = $this->validator->sanitize($data);
+        // Check if there are input errors
+        $errors = $this->validator->validatePostRequest($sanitizedData);
+        if (!empty($errors)) {
+            $responseBody = json_encode($errors);
+            $stream = new StreamFactory();
+            $response = $response
+                ->withStatus(422) // Unprocessable Entity
+                ->withHeader('Content-Type', 'application/json')
+                ->withBody($stream->createStream($responseBody));
+            return $response;
         }
 
-        // Check if price is a decimal
-        if (isset($data["price"]) && filter_var($data["price"], FILTER_VALIDATE_FLOAT) === false) {
-            $errors["price"] = "price must be a number";
+        $sku = $sanitizedData["sku"];
+        $name = $sanitizedData["name"];
+        $price = $sanitizedData["price"];
+        $type = $sanitizedData["type"];
+        $attributes = array_diff_key($sanitizedData, array_flip(["sku", "name", "price", "type"]));
+
+        // Get product type id
+        $productTypeId = $this->gateway->lookupProductTypeId($type);
+
+
+        // Convert attributes to their relevant ids
+        $attributeIds = [];
+        foreach ($attributes as $attributeKey => $attributeValue) {
+            $attributeId = $this->gateway->lookupAttributeId($attributeKey);
+            $attributeIds[$attributeId] = $attributeValue;
         }
 
-        // Check if product type is valid
-        if (!in_array($data["type"], $validTypes)) {
-            $errors["type"] = "Invalid product type";
+
+        $newData = [
+            "sku" => $sku,
+            "name" => $name,
+            "price" => $price,
+            "productTypeId" => $productTypeId,
+            "attributeIds" => $attributeIds
+        ];
+
+        $id = $this->gateway->createProduct($newData);
+        $responseBody = json_encode(['message' => 'Product created', 'id' => $id]);
+        $stream = new StreamFactory();
+        $response = $response
+            ->withStatus(201) // Created
+            ->withHeader('Content-Type', 'application/json')
+            ->withBody($stream->createStream($responseBody));
+        return $response;
+    }
+
+    private function handleDeleteRequest(array $data, Response $response): ResponseInterface
+    {
+
+        $errors = $this->validator->validateDeleteRequest($data);
+        if (!empty($errors)) {
+            $responseBody = json_encode($errors);
+            $response = $response->withStatus(400); // Bad Request
+        } else {
+            $sanitizedData = $this->validator->sanitize($data);
+            // Convert array of ids to a comma-separated string
+            $idList = implode(",", $sanitizedData['ids']);
+            // Delete products and associated records
+            $responseBody = json_encode($this->gateway->deleteProducts($idList));
+            $response = $response->withStatus(200);
         }
 
-        // Check if attributes are valid and are not empty and are decimals
-        foreach ($validAttributes as $attribute) {
-            if (array_key_exists($attribute, $data)) {
-                $value = $data[$attribute];
-                if (!isset($value) || $value === "") {
-                    $errors[$attribute] = "$attribute is required";
-                } else if (filter_var($value, FILTER_VALIDATE_FLOAT) === false) {
-                    $errors[$attribute] = "$attribute must be a number";
-                }
-            }
-        }
+        $stream = new StreamFactory();
+        $response = $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withBody(($stream->createStream($responseBody)));
 
-        // Check extra keys
-        $allowedKeys = array_merge($requiredKeys, $validAttributes);
-        $extraKeys = array_diff(array_keys($data), $allowedKeys);
-        if (!empty($extraKeys)) {
-            foreach ($extraKeys as $key) {
-                $errors[$key] = "Invalid key $key";
-            }
-        }
-
-        return $errors;
+        return $response;
     }
 }
